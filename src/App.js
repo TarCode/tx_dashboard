d6cda7795c346409dd7de09133534b9422010880
TarCode <tarcode33@gmail.com>
2018-10-27 12:55:11 +0200

    created react app, added materialize and loaded scripts dynamically

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Nav from './components/nav'
+
 class App extends Component {
+  state = {
+    scriptLoaded: false
+  }
+  componentDidMount() {
+    loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js", () => {
+      loadScript("https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js", () => {
+        this.setState({ scriptLoaded: true })
+      })
+    })
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {
+          this.state.scriptLoaded ?
+          <Nav/> :
+          <p>Loading...</p>
+        }
       </div>
     );
   }
+}
+
+function loadScript(url, callback){
+
+  let script = document.createElement("script")
+  script.type = "text/javascript";
+
+  if (script.readyState){  //IE
+      script.onreadystatechange = function(){
+          if (script.readyState === "loaded" ||
+                  script.readyState === "complete"){
+              script.onreadystatechange = null;
+              callback();
+          }
+      };
+  } else {  //Others
+      script.onload = function(){
+          callback();
+      };
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 export default App;
